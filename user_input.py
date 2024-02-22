e70be7c3beab91860d3ec585528b36c974d67482
NginaMungai <nginamungai8@gmail.com>
2024-02-22 21:22:12 +0300

    First Version

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+location = input("Please input your location: ") 
+
+personalized_message = f"Hello {name}, you are {age} years old and you live in {location}"
+print(personalized_message)
