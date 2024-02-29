2632a202dcb4364d533033f031a0c43c77597618
Ngina Mungai <nginamungai8@gmail.com>
2024-02-29 12:11:56 +0300

    first version

---
+++ Python_two.py
@@ -0,0 +1,27 @@
+#Empty list 
+my_list = []
+print(my_list)
+
+#Extend list
+my_list.extend([10, 20, 30, 40])
+print(my_list)
+
+#Insert element
+my_list.insert(1, 15)
+print(my_list)
+
+#Extend list
+my_list.extend([50, 60, 70])
+print(my_list)
+
+#Remove last element
+my_list.pop()
+print(my_list)
+
+#Sort in ascending order
+my_list.sort()
+print(my_list)
+
+index_30 = my_list.index(30)
+print(index_30)
+
